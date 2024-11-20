7569a8c5e73fb0940dd2aa6d02d1ae02336b4ad6
Jeremy Hoarau <jeremyhoarau44@gmail.com>
2024-11-20 16:07:15 +0400

    added create route for Animal

--- src/Controller/AnimalController.php
+++ src/Controller/AnimalController.php
@@ -2,17 +2,46 @@
 
 namespace App\Controller;
 
+use App\Entity\Animal;
+use App\Repository\AnimalRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class AnimalController extends AbstractController
 {
+
+    private $animalRepository;
+
+    public function __construct(AnimalRepository$animalRepository) {
+        $this->animalRepository = $animalRepository;
+    }
+
     #[Route('/animal', name: 'app_animal')]
     public function index(): Response
     {
+        $animals = $this->animalRepository->findAll();
+
         return $this->render('animal/index.html.twig', [
-            'controller_name' => 'AnimalController',
+            'animals' => $animals,
         ]);
+    }
+
+    #[Route('/animal/create', name:'app_create_animal')]
+    public function create(EntityManagerInterface $em): Response {
+
+        $animal = new Animal();
+        $animal->setName('CHAT');
+        $animal->setBirthDate(new \DateTime('2022-09-09'));
+        $animal->setHeight(100);
+        $animal->setWeight(100);
+        $animal->setSex("MALE");
+        $animal->setBreed("-");
+
+        $em->persist($animal);
+        $em->flush();
+
+        return $this->redirectToRoute('app_animal');
     }
 }
