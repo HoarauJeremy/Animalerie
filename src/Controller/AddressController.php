16596bb29b98c1cffbe36f4a8c84611c03eb675d
Jeremy Hoarau <jeremyhoarau44@gmail.com>
2024-11-21 12:12:43 +0400

    added form to address

--- src/Controller/AddressController.php
+++ src/Controller/AddressController.php
@@ -2,10 +2,12 @@
 
 namespace App\Controller;
 
+use App\Form\AddressType;
 use App\Repository\AddressRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Proxies\__CG__\App\Entity\Address;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -28,7 +30,31 @@ class AddressController extends AbstractController
         ]);
     }
 
-    #[Route('/address/create/{streetNumber}/{streetName}/{zipCode}', name:'app_address_create', methods: ['GET'])]
+    #[Route('/address/create', name:'app_address_create')]
+    public function create(Request $request, EntityManagerInterface $em): Response {
+        $address = new Address();
+
+        $formAddress = $this->createForm(AddressType::class, $address);
+
+        if ($request->isMethod('POST')) {
+            $formAddress->handleRequest($request);
+
+            if ($formAddress->isSubmitted() && $formAddress->isValid()) {
+                $addressData = $formAddress->getData();
+
+                $em->persist($addressData);
+                $em->flush();
+
+                return $this->redirectToRoute('app_address');
+            }
+        }
+
+        return $this->render('address/create.html.twig', [
+            'formAddress' => $formAddress,
+        ]);
+    }
+
+    /* #[Route('/address/create/{streetNumber}/{streetName}/{zipCode}', name:'app_address_create', methods: ['GET'])]
     public function create(EntityManagerInterface $em, int $streetNumber, string $streetName, string $zipCode): Response {
         $address = new Address();
         $address->setZipCode($zipCode);
@@ -39,5 +65,5 @@ class AddressController extends AbstractController
         $em->flush();
 
         return $this->redirectToRoute('app_address');
-    }
+    } */
 }
